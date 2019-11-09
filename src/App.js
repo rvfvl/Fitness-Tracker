deaab3849dd48a4599e1c01a76a00724aed595d7
Rafal <rafal.malewicz1@gmail.com>
2019-11-09 00:47:16 +0000

    Added basic router dom and folder structure.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import theme from "config/theme";
+import { ThemeProvider } from "styled-components";
+import SignIn from "pages/SignIn";
+import SignUp from "pages/SignUp";
+import Dashboard from "pages/Dashboard";
 
-function App() {
-  return <div>App</div>;
-}
+const App = () => {
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route exact path="/signin" component={SignIn} />
+          <Route exact path="/signup" component={SignUp} />
+        </Switch>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ src/pages/Dashboard.js
@@ -0,0 +1,8 @@
+import React from "react";
+import LoggedUserTemplate from "templates/LoggedUserTemplate";
+
+const Dashboard = () => {
+  return <LoggedUserTemplate>Dashboard</LoggedUserTemplate>;
+};
+
+export default Dashboard;
