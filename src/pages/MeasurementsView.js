de382a570247c22f2e7101de1c39e9f78bd967c1
Rafal <rafal.malewicz1@gmail.com>
2019-11-23 14:47:31 +0000

    Implemented Recharts component.

--- src/pages/MeasurementsView.js
+++ src/pages/MeasurementsView.js
@@ -2,11 +2,14 @@ import React from "react";
 import LoggedUserTemplate from "templates/LoggedUserTemplate";
 import Container from "components/Container";
 import Table from "components/Table";
+import LineChart from "components/LineChart";
 
 const MeasurementsView = () => {
   return (
     <LoggedUserTemplate>
-      <Container col="2">Measurements</Container>
+      <Container col="2">
+        <LineChart />
+      </Container>
       <Container col="2">
         <Table />
       </Container>
